9d5a33024a7d6a3ae3945a78b92addc91ebd95a6
Bastien Nocera <hadess@hadess.net>
2025-06-16 16:50:49 +0200

    Convert manifest to YAML
    This will allow us to add comments.

--- local-build.sh
+++ local-build.sh
@@ -6,6 +6,6 @@ rm -f com.sweethome3d.Sweethome3d.flatpak
 rm -rf _build ; mkdir _build
 rm -rf _repo ; mkdir _repo
 
-flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build com.sweethome3d.Sweethome3d.json --repo=_repo
+flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build com.sweethome3d.Sweethome3d.yaml --repo=_repo
 flatpak build-bundle _repo com.sweethome3d.Sweethome3d.flatpak com.sweethome3d.Sweethome3d $BRANCH
 
